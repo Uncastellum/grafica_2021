dcb4e9abbf963940012f8e96613cb2c14a296d6c
sergioge99 <sergioge99@users.noreply.github.com>
2020-11-04 13:32:47 +0100

    main

--- pr3/mainTest.cpp
+++ pr3/mainTest.cpp
@@ -13,22 +13,23 @@ using namespace std;
 
 int main(){
 
-  Camera c(Point(-10, 10, 0), Direction(0,1,0), Direction(0,0,1), Direction(10,0,0));
+  Camera c(Point(0, 0, 0), Direction(0,0,1), Direction(0,1,0), Direction(0.7,0,0));
+  Direction neg = Direction(1,0,-1);
 
   Scene scn(c);
-  shared_ptr<Object> x ( new Plane(Point(20,0,0), Direction(-1,0,0)) );         x->setRGB(RGB(255,0,0));
-  shared_ptr<Object> y ( new Plane(Point(0,20,0), Direction(0,-1,0)) );         y->setRGB(RGB(0,255,0));
-  shared_ptr<Object> yneg ( new Plane(Point(0,-20,0), Direction(0,1,0)) );   yneg->setRGB(RGB(64,255,64));
-  shared_ptr<Object> z ( new Plane(Point(0,0,20), Direction(0,0,-1)) );         z->setRGB(RGB(0,0,255));
-  shared_ptr<Object> zneg ( new Plane(Point(0,0,-20), Direction(0,0,1)) );   zneg->setRGB(RGB(64,64,255));
+  shared_ptr<Object> pared_fr ( new Plane(Point(5,0,0), Direction(1,0,0)) );         pared_fr->setRGB(RGB(22, 219, 61));
+  shared_ptr<Object> pared_iz ( new Plane(Point(5,3,0), Direction(0,1,0)) );         pared_iz ->setRGB(RGB(111, 61, 191));
+  shared_ptr<Object> pared_de ( new Plane(Point(5,-3,0), Direction(0,-1,0)) );         pared_de ->setRGB(RGB(111, 61, 191));
+  shared_ptr<Object> pared_up ( new Plane(Point(5,0,3), Direction(0,0,1)) );         pared_up ->setRGB(RGB(219, 196, 22));
+  shared_ptr<Object> pared_do ( new Plane(Point(5,0,-3), Direction(0,0,-1)) );         pared_do ->setRGB(RGB(219, 196, 22));
 
-  scn.addObj(x);
-  scn.addObj(y);
-  scn.addObj(yneg);
-  scn.addObj(z);
-  scn.addObj(zneg);
+  scn.addObj(pared_fr);
+  scn.addObj(pared_iz);
+  scn.addObj(pared_de);
+  scn.addObj(pared_up);
+  scn.addObj(pared_do);
 
-  scn.RayTracing1rppx(1024, 768);
+  scn.RayTracing1rppx(400, 400);
   scn.exportImg("loqsea.ppm");
 
 }
